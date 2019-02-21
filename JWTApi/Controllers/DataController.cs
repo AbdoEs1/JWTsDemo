0638dd9f2f5b3d6c10529337b99fae08dfb6809a
Es Salhi Abderrahmane <aessalhi@sqli-jr4h7t2.SQLI>
2019-02-21 16:13:54 +0100

    initial project

---
+++ JWTApi/Controllers/DataController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DataController : ControllerBase
+{
+    [HttpGet("getData"),Authorize]
+    public string GetData(){
+        return "data";
+    }   
+}
---
+++ JWTApi/Controllers/JWTController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JWTController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public JWTController(IConfiguration config)
+        {
+            this._config=config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("CreateToken")]
+        public IActionResult CreateToken([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+            if (Authenticate(login))
+            {
+                var tokenString = BuildToken();
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private bool Authenticate(LoginModel login)
+        {
+            if(login.UserName=="mouad")
+            return true;
+            return false;
+        }
+        private string BuildToken(){
+             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+            _config["Jwt:Issuer"],
+            expires: DateTime.Now.AddMinutes(2),
+            signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public ActionResult<RegistrationModel> Register(RegistrationModel registration)
+        {
+            return registration;
+        }
+    }
+}
